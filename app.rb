78347f51971344464c96b3d38ea1706122d1af66
Rp7rf <westhouse51@gmail.com>
2017-04-09 21:57:16 +0900

    initial commit

---
+++ app.rb
@@ -0,0 +1,54 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require './models/count.rb'
+require 'nokogiri'
+require 'open-uri'
+require './timetable'
+before do
+  if Count.all.size == 0
+    Count.create(number: 0)
+  end
+end
+
+get '/' do
+  
+  'hello'
+end
+
+def client
+  @client ||= Line::Bot::Client.new { |config|
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+  }
+end
+
+post '/callback' do
+  body = request.body.read
+
+  signature = request.env['HTTP_X_LINE_SIGNATURE']
+  unless client.validate_signature(body, signature)
+    error 400 do 'Bad Request' end
+  end
+
+  events = client.parse_events_from(body)
+  events.each { |event|
+    case event
+    when Line::Bot::Event::Message
+      case event.type
+      when Line::Bot::Event::MessageType::Text
+        message = {
+          type: 'text',
+          text: event.message['text']
+        }
+        client.reply_message(event['replyToken'], message)
+      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+        response = client.get_message_content(event.message['id'])
+        tf = Tempfile.open("content")
+        tf.write(response.body)
+      end
+    end
+  }
+
+  "OK"
+end
---
+++ timetable.rb
@@ -0,0 +1,68 @@
+
+require 'nokogiri'
+require 'open-uri'
+
+def timetable(doc, month = 0, day = 11)
+  tr = doc[2..12]
+  lectures = []
+  0.step(8, 2) do |i|
+    info = tr[i].inner_text.gsub(/\t/,'').split(/\r\n/)
+    room = tr[i+1].inner_text.gsub(/\\t/,'')
+    info = ['', '', '', ''] if info.length == 0
+    key = ['buf', 'title', 'professor', 'subtitle']
+    ary = [key, info].transpose
+    lecture = Hash[*ary.flatten]
+    lecture['room'] = room
+    lectures.push(lecture)
+  end
+  lectures
+end
+
+def getHP(month = 4, day = 1)
+  month = (month + 8) % 12
+  url = 'http://www.shiga-med.ac.jp/~hqgaku/SchoolCalendar/igaku/3/calendar_d.html'
+  page = URI.parse(url).read
+  charset = page.charset
+  
+  doc = Nokogiri::HTML.parse(page, nil, 'euc-jp')
+  td = doc.xpath('//table[@class="table_layout"]')[month].xpath('tr')[day].xpath('td')
+
+  if td.length == 3
+    # 休みの日
+    if td[2].inner_text.gsub(/[\t\n\r]/,'') == ''
+      title = '授業なし'
+    else
+      title = td[2].inner_text.gsub(/[\t\n\r]/,'').gsub('　','')
+    end
+    return {isholiday: true, title: title}
+  else
+    # 休みの日
+    isholiday = true
+    td[2..12].each do |t|
+      isholiday = false if t.inner_text.gsub(/[\t\r\n]/, '') != ''
+    end
+    return {isholiday: true, title: '授業なし'} if isholiday 
+    return {isholiday: false, classes: timetable(td)}
+  end
+end
+
+def op(month = 4, day = 1)
+  lectures = getHP(month, day)
+  msg = ""
+  msg = ''
+  if not lectures[:isholiday]
+    lectures[:classes].each_with_index do |lecture, i|
+      msg += "#{i+1}限目 #{lecture['title']} 教室 #{lecture['room']}\n - #{lecture['subtitle']} - \n(#{lecture['professor']})\n" 
+    end
+  else
+    msg = 'お休みです.'
+  end
+  msg 
+end
+t = Time.new
+p op(t.month, t.day + 1)
+p op(4,1)
+p op(4,3)
+p op(4,4)
+p op(4,6)
+p op(4,11)
