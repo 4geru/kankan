91edeebd975ea9d0a2c294dc8320546529bca09f
Rp7rf <westhouse51@gmail.com>
2017-04-12 00:11:18 +0900

    文暦の修正

--- app.rb
+++ app.rb
@@ -115,12 +115,12 @@ post '/callback' do
       m = MessageButton.new('学部選択中')
       m.pushButton('医学部',   {"data": "type=dept&department=igaku"})
       m.pushButton('看護学部', {"data": "type=dept&department=kango"})
-      client.reply_message(event['replyToken'], m.reply('学部選択', '学部を教えてください'))
+      client.reply_message(event['replyToken'], m.reply('学部選択', '初めまして！カンカンです！学部を教えてね！'))
     when Line::Bot::Event::Follow
       m = MessageButton.new('学部選択中')
       m.pushButton('医学部',   {"data": "type=dept&department=igaku"})
       m.pushButton('看護学部', {"data": "type=dept&department=kango"})
-      client.reply_message(event['replyToken'], m.reply('学部選択', '学部を教えてください'))
+      client.reply_message(event['replyToken'], m.reply('学部選択', '初めまして！カンカンです！学部を教えてね！'))
     when Line::Bot::Event::Postback
       data = Hash[URI::decode_www_form(event["postback"]["data"])]
       case data["type"]
@@ -137,8 +137,8 @@ post '/callback' do
           m2.pushButton('5年', {"data": "type=grade&department=igaku&grade=5"})
           m2.pushButton('6年', {"data": "type=grade&department=igaku&grade=6"})
           client.reply_message(event['replyToken'], m.reply([
-            m1.getButtons('医学部 > 学年選択 > 低学年', '初めまして！カンカンです！学年を教えてね！'),
-            m2.getButtons('医学部 > 学年選択 > 高学年', '初めまして！カンカンです！学年を教えてね！')
+            m1.getButtons('医学部 > 学年選択 > 低学年', '学年を教えてね！'),
+            m2.getButtons('医学部 > 学年選択 > 高学年', '学年を教えてね！')
           ]))
         when 'kango' 
           m = MessageButton.new('学年選択中')
@@ -146,7 +146,7 @@ post '/callback' do
           m.pushButton('2年', {"data": "type=grade&department=kango&grade=2"})
           m.pushButton('3年', {"data": "type=grade&department=kango&grade=3"})
           m.pushButton('4年', {"data": "type=grade&department=kango&grade=4"})
-          client.reply_message(event['replyToken'], m.reply('看護学部 > 学年選択', '初めまして！カンカンです！学年を教えてね！'))
+          client.reply_message(event['replyToken'], m.reply('看護学部 > 学年選択', '学年を教えてね！'))
         end
       when 'grade'
         channel_id = get_id(event["source"])
