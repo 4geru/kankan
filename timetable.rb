a707db8c5a5a101d9ddaca17ca1276d4381ddfc3
Rp7rf <westhouse51@gmail.com>
2017-04-12 07:48:58 +0900

    日付の修正

--- timetable.rb
+++ timetable.rb
@@ -7,7 +7,7 @@ def op(department, grade, month = 4, day = 1)
   date = t.year.to_s + '/' + month.to_s + '/' + day.to_s
   lectures = Day.where({department: department, grade: grade, date:date}).first
   dept = (department == 'igaku' ? '医学部' : '看護学部')
-  msg = "#{t.month}月#{t.day}日 #{dept} #{grade}年生"
+  msg = "#{month}月#{day}日 #{dept} #{grade}年生"
   if not lectures['isHoliday']
     eval(lectures['timetable']).each_with_index do |lecture, i|
        next if lecture['title'] == ''
