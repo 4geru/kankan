e9b0d591246e82019084e820754545be0a6ad038
Rp7rf <westhouse51@gmail.com>
2017-04-15 23:45:49 +0900

    fix bug

--- timetable.rb
+++ timetable.rb
@@ -56,7 +56,7 @@ def getEndTime(department, grade, month = 4, day = 1)
   last_period = last_lecture["period"].to_i
   p last_lecture
   time = {1 => ["8:50", "10:20"], 2 => ["10:30", "12:00"], 3 => ["13:00", "14:30"], 4 => ["14:40", "16:10"], 5 => ["16:20", "17:50"]}
-  "#{month}/#{day}(#{weekName(t.wday)})の終了時間だね！\n#{last_period}限の#{last_lecture["title"]}までで\u{23F0}#{time[last_period][1]}までだよ！"
+  "#{month}/#{day}(#{weekName(t.wday)})の終了時間だね！\n#{last_period}限の#{last_lecture["title"]}までで\n\u{23F0}#{time[last_period][1]}までだよ！"
 end
 
 def getEndWeekName(department, grade, params)
