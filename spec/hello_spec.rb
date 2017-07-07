085e25fa3c5b2239f00303f8412a8146b321021d
rp7rf <westhouse51@gmail.com>
2017-07-07 00:12:58 +0000

    specファイルの追加

---
+++ spec/hello_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../src/hello'
+
+RSpec.describe Hello do
+  it "message return hello" do
+    expect(Hello.new.message).to eq "hello"
+  end
+end
+
---
+++ src/hello.rb
@@ -0,0 +1,5 @@
+class Hello
+  def message(msg = 'hello')
+    msg
+  end
+end
