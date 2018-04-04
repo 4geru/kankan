aa50e90c97ba898a238d40088d92416e4d444933
rp7rf <westhouse51@gmail.com>
2018-04-04 23:47:50 +0000

    時間割が動くように修正

--- src/lib/getTimeTable.rb
+++ src/lib/getTimeTable.rb
@@ -7,10 +7,12 @@ def getTimeTable(department, grade, t)
   lectures = Day.find_by({department: department, grade: grade, date: date })
   dept = (department == 'igaku' ? '医学科' : '看護学科')
   msg = "#{t.month}月#{t.day}日 (#{weekName(t.wday)}) #{dept} #{grade}年生"
+  p lectures
   return "#{msg}\n#{lectures['reason']}です" if lectures['isHoliday']
   eval(lectures['timetable']).each_with_index do |lecture, i|
      next if lecture['title'] == ''
      msg += "\n#{lecture['period']}限目 #{lecture['title']} \u{1F6A9} #{lecture['room']}\n  \u{1F4D4}  #{lecture['subtitle']}\n  \u{1F468}  #{lecture['professor']}"
-   end
+  end
+  return msg
 end
 
