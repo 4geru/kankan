1e714e0684b5392bc260d619714079dcbc35f289
rp7rf <westhouse51@gmail.com>
2018-04-13 07:42:50 +0900

    blockの中にifを含んだ

--- src/lib/bus_start_at.rb
+++ src/lib/bus_start_at.rb
@@ -2,14 +2,13 @@ def bus_start_at(start)
   msg = "今日のバスの情報\n"
   t = Time.now
   trips = search_bus(start, t)
-  p trips.length
   if trips.empty?
     p 'run empty'
     t = t - t.seconds_since_midnight + 26 * 60 * 60
     msg = "明日のバスの情報\n※今日の営業は終了しています。\n"
     trips = search_bus(start, t)
+    return "今日・明日の運行がありません。" if trips.empty?
   end
-  return "今日・明日の運行がありません。" if trips.empty?
 
   msg + "出発(#{start}) >> 到着(#{trips.first.goal})\n" +
   trips[0...5].map{|trip|
