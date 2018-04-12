88291a388632b15d11e4248f5780e116ab670e5c
rp7rf <westhouse51@gmail.com>
2018-04-12 20:53:50 +0900

    models/count.rb -> models.rb

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
-require './models/count.rb'
+require './models.rb'
 require 'line/bot'
 require './src/class/MessageButton'
 require './src/class/MessageCarousel'
